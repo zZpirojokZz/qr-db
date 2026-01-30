d7195678fd414414189d3eb47a64589847072c91
igor <igor.pivnev12@gmail.com>
2026-01-30 21:55:59 +0500

    commit on mac

---
+++ ios qr-db/ios qr-db/ios_qr_dbApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_qr_dbApp.swift
+//  ios qr-db
+//
+//  Created by Анатолий Пивнев on 03.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ios_qr_dbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios qr-db/ios qr-dbTests/ios_qr_dbTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_qr_dbTests.swift
+//  ios qr-dbTests
+//
+//  Created by Анатолий Пивнев on 03.02.2026.
+//
+
+import Testing
+@testable import ios_qr_db
+
+struct ios_qr_dbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
