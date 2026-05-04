208377d0d1a535e7c00e295bc657e20e3e4624a5
Савелий <ueueueeeeee55@gmail.com>
2026-05-04 22:45:32 +0500

    бд

--- app/src/main/java/com/example/qr_db/APIserver/db.js
+++ app/src/main/java/com/example/qr_db/APIserver/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'host.docker.internal',
     database: 'SmartCheck',
     password: '123456',
     port: 5432,
